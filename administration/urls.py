9f43973a4831ffbd23a94078acac6fc6926b9da7
AaiBi <iboumocellou@yahoo.fr>
2022-03-05 16:47:25 -0500

    admin page

--- administration/urls.py
+++ administration/urls.py
@@ -10,8 +10,13 @@ urlpatterns = [
     #Devis
     path('devis_home/', views.devis_home, name='devis_home'),
     path('devis_detail/<int:quote_request_pk>', views.devis_detail, name='devis_detail'),
+    path('devis_detail/<int:quote_request_pk>', views.devis_detail, name='devis_detail'),
+    path('delete_devis/<int:quote_request_pk>', views.delete_devis, name='delete_devis'),
 
     #Movers
     path('Movers/', views.movers_home, name='movers_home'),
     path('mover_detail/<int:mover_pk>', views.mover_detail, name='mover_detail'),
+    path('mover_devis/<int:mover_pk>', views.mover_devis, name='mover_devis'),
+    path('mover_active_unactive/<int:mover_pk>', views.mover_active_unactive, name='mover_active_unactive'),
+
 ]
--- administration/views.py
+++ administration/views.py
@@ -7,7 +7,8 @@ from django.db.models import Count
 from django.shortcuts import render, redirect, get_object_or_404
 
 from base_app.forms import Mover_Form
-from base_app.models import Mover, Country, Quote_Request, Moving_Type1, Moving_Type2, Mover_Quote_Request
+from base_app.models import Mover, Country, Quote_Request, Moving_Type1, Moving_Type2, Mover_Quote_Request, \
+    Quote_Request_Rejected
 
 
 def admin_login(request):
@@ -56,10 +57,29 @@ def devis_home(request):
     moving_type1 = Moving_Type1.objects.all()
     moving_type2 = Moving_Type2.objects.all()
     distributed_requests = Mover_Quote_Request.objects.values('quote_request_id').annotate(Count('id')).order_by()
+    quote_requests_rejected = Quote_Request_Rejected.objects.filter(mover_quote_request__rejected=True)
+    number_quote_requests_rejected = Quote_Request_Rejected.objects.filter(mover_quote_request__rejected=True).count()
+
+    if 'search' in request.POST:
+        ref = request.POST.get('ref')
+        if Quote_Request.objects.filter(ref=ref):
+            quote_request = get_object_or_404(Quote_Request, ref=ref)
+
+            if quote_request:
+                return render(request, 'administration/devis/devis_home.html', {'quote_request': quote_request})
+
+        else:
+            messages.error(request, 'Cette référence n\'existe pas !')
+            return render(request, 'administration/devis/devis_home.html')
+
     return render(request, 'administration/devis/devis_home.html', {'countries': countries, 'quote_requests':
                     quote_requests, 'moving_type1': moving_type1, 'moving_type2': moving_type2, 'mover_quote_requests':
                                                                     mover_quote_requests, 'distributed_requests':
-                                                                    distributed_requests})
+                                                                    distributed_requests,
+                                                                    'number_quote_requests_rejected':
+                                                                    number_quote_requests_rejected,
+                                                                    'quote_requests_rejected':
+                                                                    quote_requests_rejected})
 
 
 @login_required
@@ -72,6 +92,12 @@ def devis_detail(request, quote_request_pk):
     users = User.objects.all()
     mover_quote_requests = Mover_Quote_Request.objects.filter(quote_request_id=quote_request_pk)
     mover_quote_requests_number = Mover_Quote_Request.objects.filter(quote_request_id=quote_request_pk).count()
+    number_quote_requests_rejected = Quote_Request_Rejected.objects.filter(mover_quote_request__rejected=True,
+                                                                           mover_quote_request__quote_request__id=
+                                                                           quote_request_pk).count()
+    quote_requests_rejected = Quote_Request_Rejected.objects.filter(mover_quote_request__rejected=True,
+                                                                           mover_quote_request__quote_request__id=
+                                                                           quote_request_pk)
 
     if request.method == 'GET':
         return render(request, 'administration/devis/devis_detail.html', {'devis': devis, 'countries': countries,
@@ -79,7 +105,24 @@ def devis_detail(request, quote_request_pk):
                                                                           : moving_type2, 'movers': movers, 'users':
                                                                           users, 'mover_quote_requests_number':
                                                                           mover_quote_requests_number,
-                                                                          'mover_quote_requests': mover_quote_requests})
+                                                                          'mover_quote_requests': mover_quote_requests
+                                                                          , 'number_quote_requests_rejected':
+                                                                          number_quote_requests_rejected,
+                                                                          'quote_requests_rejected':
+                                                                          quote_requests_rejected})
+
+
+@login_required
+def delete_devis(request, quote_request_pk):
+    quote_request = get_object_or_404(Quote_Request, pk=quote_request_pk)
+
+    if request.method == 'GET':
+        return render(request, 'administration/devis/delete_devis.html',
+                      {'quote_request': quote_request})
+    if request.method == 'POST':
+        quote_request.delete()
+        messages.success(request, 'Suppression effectuée !')
+        return redirect('devis_home')
 ###################################################  END DEVIS    #####################################################
 
 
@@ -90,22 +133,32 @@ def movers_home(request):
     countries = Country.objects.all()
     moving_type1 = Moving_Type1.objects.all()
     moving_type2 = Moving_Type2.objects.all()
-    movers = Mover.objects.all()
     movers_activated = Mover.objects.filter(activated=True)
     movers_unactivated = Mover.objects.filter(activated=False)
     movers_unactivated_count = Mover.objects.filter(activated=False).count()
     users = User.objects.all()
+    form = Mover_Form()
 
-    if request.method == 'GET':
-        form = Mover_Form()
-        return render(request, 'administration/movers/movers_home.html', {'countries': countries,
-                                                                          'moving_type1': moving_type1, 'moving_type2'
-                                                                          : moving_type2, 'movers': movers, 'users':
-                                                                          users, 'movers_unactivated':
-                                                                          movers_unactivated, 'movers_unactivated_count'
-                                                                          : movers_unactivated_count, 'form': form,
-                                                                          'movers_activated': movers_activated})
     if request.method == 'POST':
+
+        if 'search' in request.POST:
+            ref = request.POST.get('ref')
+            if Mover.objects.filter(ref=ref):
+                mover = get_object_or_404(Mover, ref=ref)
+                return render(request, 'administration/movers/movers_home.html',
+                                  {'countries': countries, 'moving_type1': moving_type1, 'moving_type2': moving_type2,
+                                   'users': users, 'movers_unactivated': movers_unactivated, 'movers_unactivated_count':
+                                      movers_unactivated_count, 'movers_activated': movers_activated, 'mover': mover})
+            elif Mover.objects.filter(company_name=ref):
+                mover = get_object_or_404(Mover, company_name=ref)
+                return render(request, 'administration/movers/movers_home.html',
+                                  {'countries': countries, 'moving_type1': moving_type1, 'moving_type2': moving_type2,
+                                   'users': users, 'movers_unactivated': movers_unactivated, 'movers_unactivated_count':
+                                      movers_unactivated_count, 'movers_activated': movers_activated, 'mover': mover})
+            else:
+                messages.error(request, 'Ce nom n\'existe pas !')
+                return render(request, 'administration/movers/movers_home.html')
+
         if request.POST.get('mover_id'):
             mover_info = get_object_or_404(Mover, pk=request.POST.get('mover_id'))
             form = Mover_Form(request.POST, instance=mover_info)
@@ -114,30 +167,90 @@ def movers_home(request):
                 messages.success(request, 'Profil activé !')
                 return redirect('movers_home')
 
+    return render(request, 'administration/movers/movers_home.html', {'countries': countries,
+                                                                      'moving_type1': moving_type1, 'moving_type2'
+                                                                      : moving_type2, 'users':
+                                                                          users, 'movers_unactivated':
+                                                                          movers_unactivated, 'movers_unactivated_count'
+                                                                      : movers_unactivated_count, 'form': form,
+                                                                      'movers_activated': movers_activated})
 
 @login_required
 def mover_detail(request, mover_pk):
     mover = get_object_or_404(Mover, pk=mover_pk)
-
     countries = Country.objects.all()
     moving_type1 = Moving_Type1.objects.all()
     moving_type2 = Moving_Type2.objects.all()
     movers = Mover.objects.all()
     users = User.objects.all()
+    number_mover_quote_requests = Mover_Quote_Request.objects.filter(mover_id=mover_pk).count()
 
-    if request.method == 'GET':
-        return render(request, 'administration/movers/mover_detail.html', {'mover': mover, 'countries': countries,
-                                                                          'moving_type1': moving_type1, 'moving_type2'
-                                                                          : moving_type2, 'movers': movers, 'users':
-                                                                          users})
+    return render(request, 'administration/movers/mover_detail.html', {'mover': mover, 'countries': countries,
+                                                                       'moving_type1': moving_type1, 'moving_type2'
+                                                                       : moving_type2, 'movers': movers, 'users':
+                                                                           users, 'number_mover_quote_requests':
+                                                                       number_mover_quote_requests})
+
+
+@login_required
+def mover_active_unactive(request, mover_pk):
+    mover = get_object_or_404(Mover, pk=mover_pk)
+    countries = Country.objects.all()
+    moving_type1 = Moving_Type1.objects.all()
+    moving_type2 = Moving_Type2.objects.all()
+    movers = Mover.objects.all()
+    users = User.objects.all()
+    form = Mover_Form(request.POST)
 
     if request.method == 'POST':
-        if request.POST.get('mover_id'):
-            mover_info = get_object_or_404(Mover, pk=request.POST.get('mover_id'))
-            form = Mover_Form(request.POST, instance=mover_info)
+        mover_info = get_object_or_404(Mover, pk=mover_pk)
+        form = Mover_Form(request.POST, instance=mover_info)
+        form.country = Country.objects.filter(id=request.POST.get('country_id'))
+        moving_type1 = Moving_Type1.objects.filter(id=request.POST.get('moving_type1_id'))
+
+        if request.POST.get('activated') == 'True':
             if form.is_valid():
                 form.save()
                 messages.success(request, 'Profil activé !')
                 return redirect('movers_home')
 
+        if request.POST.get('activated') == 'False':
+            if form.is_valid():
+                form.save()
+                messages.error(request, 'Profil désactivé !')
+                return redirect('movers_home')
+
+    return render(request, 'administration/movers/mover_active_unactive.html', {'mover': mover, 'countries': countries,
+                                                                       'moving_type1': moving_type1, 'moving_type2'
+                                                                       : moving_type2, 'movers': movers, 'users':
+                                                                           users, 'form': form})
+
+
+@login_required
+def mover_devis(request, mover_pk):
+    mover = get_object_or_404(Mover, pk=mover_pk)
+    mover_quote_requests = Mover_Quote_Request.objects.filter(mover_id=mover_pk)
+    number_mover_quote_requests = Mover_Quote_Request.objects.filter(mover_id=mover_pk).count()
+
+    if 'search' in request.POST:
+        ref = request.POST.get('ref')
+        if Quote_Request.objects.filter(ref=ref):
+            quote_request = get_object_or_404(Quote_Request, ref=ref)
+            mover_quote_request = Mover_Quote_Request.objects.filter(quote_request_id=quote_request.id, mover_id=mover.
+                                                                     id).last()
+            if mover_quote_request:
+                return render(request, 'administration/movers/mover_devis.html', {'mover': mover, 'mover_quote_request':
+                                    mover_quote_request, 'number_mover_quote_requests': number_mover_quote_requests})
+            else:
+                messages.error(request, 'Cette demande de devis ne vous est pas destinée !')
+                return render(request, 'administration/movers/mover_devis.html')
+
+        else:
+            messages.error(request, 'Cette référence n\'existe pas !')
+            return render(request, 'administration/movers/mover_devis.html')
+
+    return render(request, 'administration/movers/mover_devis.html', {'mover': mover, 'mover_quote_requests':
+                                                                      mover_quote_requests,
+                                                                      'number_mover_quote_requests':
+                                                                      number_mover_quote_requests})
 ##################################################   END Movers    ####################################################
